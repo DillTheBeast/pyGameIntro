d7211ef0a8a00a2d681a92b3bdca035959a7f048
DillTheBeast <dillonmaltese@gmail.com>
2023-07-19 09:02:18 -0400

    sdf

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ WIDTH, HEIGHT = 800, 600
 PADDLE_WIDTH, PADDLE_HEIGHT = 15, 80
 BALL_SIZE = 15
 PADDLE_SPEED = 0
-BALL_SPEED  = 3
+BALL_SPEED  = 0
 
 score1 = 0
 score2 = 0
@@ -20,13 +20,17 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 color_light = (170,170,170)
 color_dark = (100,100,100)
+
 myFont = pygame.font.SysFont("Times New Roman", 60)
-buttonFont = pygame.font.SysFont("Times New Roman", 120)
 text1 = myFont.render(str(score1), False, WHITE)
 text2 = myFont.render(str(score2), False, WHITE)
+
+buttonFont = pygame.font.SysFont("Times New Roman", 35)
 button = buttonFont.render('START' , True , WHITE)
+buttonClick = False
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Pong")
 clock = pygame.time.Clock()
 #First = Left and right set
 #Second = Up and Down set
@@ -40,6 +44,9 @@ paddle2_speed = [0, PADDLE_SPEED]
 ball_speed = [BALL_SPEED, BALL_SPEED]
 
 running = True
+done = False
+done1 = False
+right = True
 
 #score = int(input("What do you want the score to be when the player wins?\n"))
 scoreMax = 8
@@ -52,10 +59,16 @@ while running:
             pygame.quit()
             running = False
 
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if WIDTH/2 <= mouse[0] <= WIDTH/2+140 and HEIGHT/2 <= mouse[1] <= HEIGHT/2+40:
-                pygame.quit()
-
+        if event.type == pygame.MOUSEBUTTONDOWN:         
+            #if the mouse is clicked on the
+            # button the game is terminated
+            if WIDTH/2-70 <= mouse[0] <= WIDTH/2+140 and HEIGHT/2-20 <= mouse[1] <= HEIGHT/2+40:
+                BALL_SPEED = 3
+                ball_speed[0] = 3
+                ball_speed[1] = 3
+                buttonClick = True
+            
+               
         if event.type == pygame.KEYDOWN:
             PADDLE_SPEED = 3
             if event.key == pygame.K_w:
@@ -84,27 +97,29 @@ while running:
     if paddle2.bottom > HEIGHT:
         paddle2.bottom = HEIGHT
 
-    ball.move_ip(ball_speed)
+    if buttonClick:
+        if right:
+            ball.move_ip(ball_speed)
+        else:
+            ball.move_ip( ball_speed)
 
     if ball.top < 0 or ball.bottom > HEIGHT:
         ball_speed[1] *=- 1
     if ball.colliderect(paddle1) or ball.colliderect(paddle2):
         ball_speed[0] *=- 1
-        ball_speed[0] += 0.2
-        ball_speed[1] += 0.2
-        print("ball0", ball_speed[0])
-        print("ball1", ball_speed[1])
-        # if ball.colliderect(paddle1):
-        #     ball_speed[0] -= 0.2
-        #     ball_speed[1] -= 0.2
-        # else:
-        #     ball_speed[0] += 0.2
-        #     ball_speed[1] += 0.2
+        if abs(ball_speed[0]) == ball_speed[0]:
+            #Positive number
+            ball_speed[0] += 0.2
+            ball_speed[1] += 0.2
+        else:
+            ball_speed[0] -= 0.2
+            ball_speed[1] -= 0.2
 
     if ball.left < 0:
         ball.center = (WIDTH/2, HEIGHT/2)
         ball_speed[0] *=- 1
         score1 += 1
+        right = not right
         text1 = myFont.render(str(score1), False, WHITE)
         BALL_SPEED = 3
 
@@ -112,26 +127,38 @@ while running:
         ball.center = (WIDTH/2, HEIGHT/2)
         ball_speed[0] *=- 1
         score2 += 1
+        right = not right
         text2 = myFont.render(str(score2), False, WHITE)
         ball_speed[0] = 3
         ball_speed[1] = 3
 
     # fill the screen with a color to wipe away anything from last frame
-    mouse = pygame.mouse.get_pos()
-    if WIDTH/2 <= mouse[0] <= WIDTH/2+140 and HEIGHT/2 <= mouse[1] <= HEIGHT/2+40:
-        pygame.draw.rect(screen,color_light,[WIDTH/2,HEIGHT/2,140,40])
-          
-    else:
-        pygame.draw.rect(screen,color_dark,[WIDTH/2,HEIGHT/2,140,40])
-    screen.blit(button, (WIDTH/2+50,HEIGHT/2))
     screen.fill(BLACK)
+    mouse = pygame.mouse.get_pos()
+    if not buttonClick:
+        if WIDTH/2-70 <= mouse[0] <= WIDTH/2+140 and HEIGHT/2-20 <= mouse[1] <= HEIGHT/2+40:
+            pygame.draw.rect(screen,color_light,[WIDTH/2 - 70,HEIGHT/2 - 20, 140, 40])        
+        else:
+            pygame.draw.rect(screen,color_dark,[WIDTH/2 - 70,HEIGHT/2 - 20, 140, 40])
+    else:
+        if not done1:
+            pygame.display.update()
+            done1 = True
+
     screen.blit(text1, (0, 0))
     screen.blit(text2, (770, 0))
+    if not buttonClick:
+        screen.blit(button , (WIDTH/2 - 70 + 35/2,HEIGHT/2 - 20))
+    else:
+        if not done:
+            pygame.display.update()
+            done = True
+
     pygame.draw.rect(screen, WHITE, paddle1)
     pygame.draw.rect(screen, WHITE, paddle2)
     pygame.draw.rect(screen, WHITE, ball)
-    pygame.display.update()
-    if score1 or score2 == 8:
+    #pygame.display.updatew()
+    if score1 == 8 or score2 == 8:
         quit()
 
     # RENDER YOUR GAME HERE
