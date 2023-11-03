1ac41a9ded056feef84c1ce57078c1b994a64b6d
DillTheBeast <dillonmaltese@gmail.com>
2023-11-03 18:39:01 -0400

    asd

--- Ball.py
+++ Ball.py
@@ -34,3 +34,43 @@ class Ball:
     def updatePos(self):
         self.yPos += self.ySpeed
         self.xPos += self.xSpeed
+
+
+class Square:
+    def __init__(self, xPos, yPos, size, color, mass, retention, ySpeed, xSpeed, id):
+        self.xPos = xPos
+        self.yPos = yPos
+        self.size = size  # changed from radius to size
+        self.color = color
+        self.mass = mass
+        self.retention = retention
+        self.ySpeed = ySpeed
+        self.xSpeed = xSpeed
+        self.id = id
+        self.rect = None  # changed from circle to rect
+    
+    def draw(self, screen):
+        self.rect = pygame.draw.rect(screen, self.color, (self.xPos, self.yPos, self.size, self.size))
+
+    def check_gravity(self, HEIGHT, wall_thickness, gravity, bounceStop):
+        if self.yPos + self.size > HEIGHT - wall_thickness:
+            if self.ySpeed > bounceStop:
+                self.ySpeed = self.ySpeed * -1 * self.retention
+                self.yPos = HEIGHT - wall_thickness - self.size  # set position to just above the ground
+            else:
+                if abs(self.ySpeed) <= bounceStop:
+                    self.ySpeed = 0
+        else:
+            self.ySpeed += gravity
+
+        return self.ySpeed
+
+
+    def updatePos(self, HEIGHT, wall_thickness):
+        self.yPos += self.ySpeed
+        # Check if the square is below the ground after the update
+        if self.yPos + self.size > HEIGHT - wall_thickness:
+            self.yPos = HEIGHT - wall_thickness - self.size
+
+        self.xPos += self.xSpeed
+
--- BouncingBall.py
+++ BouncingBall.py
@@ -1,5 +1,6 @@
 import pygame
 from Ball import Ball
+from Ball import Square
 
 pygame.init()
 
@@ -15,6 +16,8 @@ gravity = 0.5
 bounceStop = 0.3
 ball1 = Ball(50, 50, 30, 'blue', 100, .9, 0, 0, 1)
 ball2 = Ball(500, 500, 50, 'green', 300, .9, 0, 0, 2)
+square1 = Square(30, 300, 30, 'blue', 100, .9, 0, 0, 1)
+square2 = Square(500, 500, 50, 'green', 300, .9, 0, 0, 2)
 
 def draw_walls():
     left = pygame.draw.line(screen, 'white', (0, 0), (0, HEIGHT), wall_thickness)
@@ -39,6 +42,12 @@ while run:
     ball2.updatePos()
     ball1.ySpeed = ball1.check_gravity(HEIGHT, wall_thickness, gravity, bounceStop)
     ball2.ySpeed = ball2.check_gravity(HEIGHT, wall_thickness, gravity, bounceStop)
+    square1.draw(screen)
+    square2.draw(screen)
+    square1.updatePos(HEIGHT, wall_thickness)
+    square2.updatePos(HEIGHT, wall_thickness)
+    square1.ySpeed = ball1.check_gravity(HEIGHT, wall_thickness, gravity, bounceStop)
+    square2.ySpeed = ball2.check_gravity(HEIGHT, wall_thickness, gravity, bounceStop)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
